3cbf680c5628ea4100ef199fa8e73fd0465f39ae
Breno Abreu <57421620+breno-abreu@users.noreply.github.com>
2021-06-22 10:20:43 -0300

    Adicionada modificação que cria arquipélagos, com terreno que não toca as bordas.

--- PTG/src/ptg/generators/Generator.java
+++ PTG/src/ptg/generators/Generator.java
@@ -46,14 +46,21 @@ public class Generator {
         double dist = 0;
         double cx = (double)width / 2 / (double)width - 0.5;
         double cy = (double)heidth / 2 / (double)heidth - 0.5;
+        int limiteInferior = 320;
+        int limiteSuperior = 490;
 
         for(int i = 0; i < heidth; i++){
             for(int j = 0; j < width; j++){
                 nx = (double)j / width - 0.5;
                 ny = (double)i / heidth - 0.5;
 
-                dist = Math.sqrt(Math.pow(nx - cx, 2) + Math.pow(ny - cy, 2));
-                dist = Math.pow(dist, 0.7);
+                /*dist = Math.sqrt(Math.pow(nx - cx, 2) + Math.pow(ny - cy, 2));
+                dist = Math.pow(dist, 0.6);*/
+
+                // Calcula a distância do centro de um ponto em um quadrado, usado para gerar o gradiente
+                int auxi = i - heidth / 2;
+                int auxj = j - width / 2;
+                dist = Math.max(Math.abs(auxi), Math.abs(auxj));
 
                 for(int o = 1; o <= Math.pow(2, octaves - 1); o *= 2){
                     res += (double)1 / o * noise(o * nx, o * ny);
@@ -61,7 +68,20 @@ public class Generator {
                 }
 
                 res = res / div;
-                noiseMatrix[i][j] = (1 + res - dist) / 2;
+
+                // A partir da ideia de um gradiente retangular, modifica o terreno para evitar que as bordas do quadrado contenham terra
+                // Ver https://code2d.wordpress.com/2020/07/21/island-gradient/ A ideia é a mesma, mas feita de forma muito menos complicada
+                if(dist > limiteSuperior){
+                    res = 0;
+                }
+                else if(dist >= limiteInferior && dist <= limiteSuperior){
+                    double aux = dist - limiteInferior;
+                    aux /= (limiteSuperior - limiteInferior);
+                    aux = 1 - aux;
+                    res *= aux;
+                }
+                //noiseMatrix[i][j] = (1 + res - dist) / 2;
+                noiseMatrix[i][j] = res;
                 res = 0;
                 div = 0;
             }
